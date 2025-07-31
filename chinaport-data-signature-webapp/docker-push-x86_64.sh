5b7252d7fe9b4d13b3675b755ae68a58afaa42dd
julian <julxxy@outlook.com>
2025-07-31 19:19:02 +0800

    chore(webapp): 删除旧版docker推送脚本并添加多架构构建脚本
    删除单架构docker推送脚本(docker-push-arm64.sh和docker-push-x86_64.sh)，新增支持多平台构建推送的docker_build_and_push_multiarch.sh脚本

--- chinaport-data-signature-webapp/docker-push-x86_64.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TAG="1.2.0"
-docker stop chinaport-data-signature && docker rm -f chinaport-data-signature
-docker rmi -f weasleyj/chinaport-data-signature:${TAG}
-docker build -t weasleyj/chinaport-data-signature:${TAG} . -f ./Dockerfile
-docker login -u weasleyj
-docker push weasleyj/chinaport-data-signature:${TAG}
-exit 0
