49311c5aa93544bcb1585bc68ecac39d8819a188
julian <julxxy@outlook.com>
2025-11-28 14:30:53 +0800

    chore: update deploy script to use dynamic user path for settings

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ else
   echo "未检测到 sptn 命令，跳过执行"
 fi
 
-SETTINGS="/Users/julian/Development/program/apache-maven/conf/settings-maven-central.xml"
+SETTINGS="/Users/$USER/Development/program/apache-maven/conf/settings-maven-central.xml"
 MODULE="chinaport-data-signature-data-model"
 
 clear && mvn deploy -pl :$MODULE -am --settings $SETTINGS
