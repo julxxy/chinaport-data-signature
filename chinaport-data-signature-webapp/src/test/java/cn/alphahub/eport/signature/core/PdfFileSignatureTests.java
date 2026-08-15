37e710b78e40298cdd49eede38538036c408676e
julian <julxxy@outlook.com>
2026-08-15 17:25:12 +0800

    feat: 海关PDF Base64内容加签+验证签名结果

--- chinaport-data-signature-webapp/src/test/java/cn/alphahub/eport/signature/core/PdfFileSignatureTests.java
+++ chinaport-data-signature-webapp/src/test/java/cn/alphahub/eport/signature/core/PdfFileSignatureTests.java
@@ -2,6 +2,7 @@ package cn.alphahub.eport.signature.core;
 
 import cn.alphahub.eport.signature.entity.UkeyRequest;
 import cn.alphahub.eport.signature.entity.UkeyResponse.Args;
+import cn.hutool.core.codec.Base64;
 import cn.hutool.json.JSONUtil;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -73,6 +74,30 @@ class PdfFileSignatureTests {
 
         Map<String, Object> verifyArgsMap = new LinkedHashMap<>();
         verifyArgsMap.put("filePath", pdfFile.toString());
+        Args verifyArgs = signHandler.getUkeyResponseArgs(new UkeyRequest(PDF_CHECK_SIGN_METHOD, verifyArgsMap));
+        System.err.println("PDF验签结果: " + JSONUtil.toJsonStr(verifyArgs));
+    }
+
+    @Test
+    @DisplayName("海关PDF Base64内容加签+验证签名结果")
+    void signPdfFileContent() throws Exception {
+        Path pdfFile = resolvePdfFile();
+        String fileContent = Base64.encode(Files.readAllBytes(pdfFile));
+        String datetiem = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+
+        Map<String, Object> signArgsMap = new LinkedHashMap<>();
+        signArgsMap.put("fileContent", fileContent);
+        signArgsMap.put("datetiem", datetiem);
+        signArgsMap.put("Edoc_code", EDOC_CODE);
+        signArgsMap.put("passwd", DEFAULT_PASSWORD);
+
+        Args signArgs = signHandler.getUkeyResponseArgs(new UkeyRequest(PDF_ADD_SIGN_METHOD, signArgsMap));
+        System.err.println("PDF加签结果: " + JSONUtil.toJsonStr(signArgs));
+        System.err.println("X509Certificate: " + certHandler.getX509Certificate(CertificateHandler.SING_DATA_METHOD));
+
+        String signedPdfBase64 = signArgs.getData().isEmpty() ? fileContent : signArgs.getData().get(0);
+        Map<String, Object> verifyArgsMap = new LinkedHashMap<>();
+        verifyArgsMap.put("fileContent", signedPdfBase64);
         Args verifyArgs = signHandler.getUkeyResponseArgs(new UkeyRequest(PDF_CHECK_SIGN_METHOD, verifyArgsMap));
         System.err.println("PDF验签结果: " + JSONUtil.toJsonStr(verifyArgs));
     }
